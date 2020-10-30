6167bd8f1a8f714a7d90eb215f7620d066581fe9
sikk806 <sikk806@gmail.com>
2020-10-30 17:55:54 +0900

    getName, todo

--- getting.js
+++ getting.js
@@ -5,14 +5,26 @@ const greeting = document.querySelector(".js-greetings");
 const USER_LS = "currentUser";
 const SHOWING_CN = "showing";
 
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
 function askForName() {
-    form.classList.add(SHOWING_CH);
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
 }
 
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello ${text}`;
+    greeting.innerText = `메롱 ${text}`;
 }
 
 function loadname() {
@@ -20,7 +32,7 @@ function loadname() {
     if(currentUser === null) {
         askForName();
     } else {
-        paintGreeding(currentUser);
+        paintGreeting(currentUser);
     }
 }
 
---
+++ todo.js
@@ -0,0 +1,3 @@
+const toDOForm = document.querySelector(".js-toDoForm");
+const toDoInput = form.querySelector("input");
+const toDoList = document.querySelector(".js-toDoList");
