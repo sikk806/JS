eb317ea05c7ec9e765a17c462da4bcfccb1daf32
sikk806 <sikk806@gmail.com>
2020-11-01 20:21:29 +0900

    make todo

--- todo.js
+++ todo.js
@@ -1,3 +1,50 @@
-const toDOForm = document.querySelector(".js-toDoForm");
-const toDoInput = form.querySelector("input");
+const toDoForm = document.querySelector(".js-toDoForm");
+const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text) {
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerText = "❌";
+    span.innerText = text;
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    li.id  = newId;
+    toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+}
+
+function loadToDos() {
+    const toDos = localStorage.getItem(TODOS_LS);
+    // if(toDos !== null) {
+
+    // }
+}
+
+function init() {
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
